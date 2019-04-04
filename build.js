0258773e4e78c5b406062c863ca2dc9948a7f9a5
whats-mick <9634573+mick-whats@users.noreply.github.com>
2019-04-04 11:59:43 +0900

    fix

--- build.js
+++ build.js
@@ -2,17 +2,26 @@ const glob = require('tiny-glob');
 const marked = require('marked');
 const fs = require('fs');
 const template = require('./template');
-const baseUrl = 'https://mick-whats-images.netlify.com';
+
+const BASE_URL = 'https://mick-whats-images.netlify.com';
+const IMAGE_DIR_NAME = 'assets/';
+
 (async function() {
   let files = await glob('**/*.{png,jpeg,jpg}');
+  /*
+  [ 'images/boip/tutorial01.png',
+  'images/jxb/jxb_logo.png',
+  'images/jxb/jxb_ss_01.png' ]
+  */
   let md = `# mick-whats images
 
   [![Netlify Status](https://api.netlify.com/api/v1/badges/71321e3e-3db9-4b91-9dc3-16cfc4c137f7/deploy-status)](https://app.netlify.com/sites/sad-nobel-60023c/deploys)
 
 
   `;
-  files.forEach(_path => {
-    const url = `${baseUrl}/${_path}`;
+  files.forEach(basePath => {
+    const _path = basePath.replace(IMAGE_DIR_NAME, '');
+    const url = `${BASE_URL}/${_path}`;
     const text = [
       `## ${_path}`,
       '### image',
@@ -37,6 +46,6 @@ const baseUrl = 'https://mick-whats-images.netlify.com';
     md += text.join('\n\n');
   });
   const html = template.replace('<!-- page content -->', marked(md));
-  fs.writeFileSync('README.md', md);
-  fs.writeFileSync('index.html', html);
+  // fs.writeFileSync('README.md', md);
+  fs.writeFileSync(IMAGE_DIR_NAME + 'index.html', html);
 })();
--- template.js
+++ template.js
@@ -5,9 +5,9 @@ module.exports = `
 		<meta charset="utf-8">
 		<meta name="viewport" content="width=device-width,initial-scale=1">
 		<title>mick-whats-images</title>
-		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/github-markdown-css/3.0.1/github-markdown.min.css">
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.3/css/uikit.min.css">
 	</head>
-	<body>
+	<body class="uk-container">
 		<!-- page content -->
 			
 	</body>
