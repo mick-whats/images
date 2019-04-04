dd38763cc764f35e6187909caf5410724dac860c
whats-mick <9634573+mick-whats@users.noreply.github.com>
2019-04-04 15:29:50 +0900

    remove package: marked

--- build.js
+++ build.js
@@ -1,5 +1,4 @@
 const glob = require('tiny-glob');
-const marked = require('marked');
 const fs = require('fs');
 const template = require('./template');
 
@@ -7,31 +6,18 @@ const BASE_URL = 'https://mick-whats-images.netlify.com';
 const IMAGE_DIR_NAME = 'assets/';
 
 (async function() {
-  let files = await glob('**/*.{png,jpeg,jpg}');
-  /*
-  [ 'images/boip/tutorial01.png',
-  'images/jxb/jxb_logo.png',
-  'images/jxb/jxb_ss_01.png' ]
-  */
-  let md = `# mick-whats images
-
-  [![Netlify Status](https://api.netlify.com/api/v1/badges/71321e3e-3db9-4b91-9dc3-16cfc4c137f7/deploy-status)](https://app.netlify.com/sites/sad-nobel-60023c/deploys)
-
-
-  `;
+  const files = await glob('**/*.{png,jpeg,jpg}');
+  let list = '';
   files.forEach(basePath => {
     const _path = basePath.replace(IMAGE_DIR_NAME, '');
-    const url = `${BASE_URL}/${_path}`;
-    const text = [
-      `<a href="${url}"><img src="${_path}" width="150px"></a>`,
-      `[${_path}](${url})`,
-      '---',
-      ''
-    ];
-
-    md += text.join('\n\n');
+    list += `
+    <li>
+      <a href="${_path}">
+        <img src="${_path}" width="150px">
+        <span class='uk-padding'>${_path}</span>
+      </a>
+    </li>`;
   });
-  const html = template.replace('<!-- page content -->', marked(md));
-  // fs.writeFileSync('README.md', md);
+  const html = template.replace('<!-- page content -->', list);
   fs.writeFileSync(IMAGE_DIR_NAME + 'index.html', html);
 })();
--- template.js
+++ template.js
@@ -8,8 +8,14 @@ module.exports = `
 		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.3/css/uikit.min.css">
 	</head>
 	<body class="uk-container">
-		<!-- page content -->
-			
+		<h1>mick-whats images</h1>
+		<a href="https://app.netlify.com/sites/sad-nobel-60023c/deploys"><img src="https://api.netlify.com/api/v1/badges/71321e3e-3db9-4b91-9dc3-16cfc4c137f7/deploy-status" alt="Netlify Status"></a>
+		<p>
+			<a href="https://github.com/mick-whats/images/"><strong>Repository: https://github.com/mick-whats/images/</strong></a>
+		</p>
+		<ul class="uk-list uk-list-divider">
+			<!-- page content -->
+		</ul>
 	</body>
 
 </html>`;
